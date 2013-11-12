aa1ca198b3d46fa9448b4bf1ed57a59392c9f8f3
Neil Freeman <nfreeman@dot.nyc.gov>
2013-11-12 14:25:40 -0500

    downgrade to older version of js libraries

--- street-view-comments/street_view_comments.php
+++ street-view-comments/street_view_comments.php
@@ -3,9 +3,8 @@
 Plugin Name: Street View Comments
 Plugin URI:
 Description: Allows for comments to be associated with Google Street View images along a street.
-Version: 0.3
-Author: Chris Abraham
-Author URI: http://cjyabraham.com
+Version: 0.4
+Author: Chris Abraham and Neil Freeman
 */
 
 // enqueue scripts. called in the shortcode handler (svc_show_mapper)
@@ -14,15 +13,15 @@ function fitzgerald_scripts($localization) {
   wp_enqueue_script( 'gmaps', 'http://maps.googleapis.com/maps/api/js?sensor=false' );
   wp_enqueue_script( 'jquery-ui-custom', plugins_url( 'js/lib/jquery-ui-1.8.21.custom.min.js' , __FILE__ ), array('jquery'), '1.8.21' );
   wp_enqueue_script( 'jquery-sparkline', plugins_url( 'js/lib/jquery.sparkline-2.0.min.js' , __FILE__ ), array('jquery'). '2.0' );
-  wp_enqueue_script( 'json2', plugins_url( 'js/lib/json2.min.js' , __FILE__ ));
-  wp_enqueue_script( 'underscore', plugins_url( 'js/lib/underscore-1.3.3.min.js' , __FILE__ ), array(), '1.3.3' );
+  wp_enqueue_script( 'json2.a', plugins_url( 'js/lib/json2.min.js' , __FILE__ ));
+  wp_enqueue_script( 'underscore.1.3.3', plugins_url( 'js/lib/underscore-1.3.3.min.js' , __FILE__ ), array(), '1.3.3' );
   // doesn't work with newer backbone
-  wp_enqueue_script( 'backbone.0.9.2', plugins_url( 'js/lib/backbone-0.9.2.min.js' , __FILE__ ), array('underscore'), '0.9.2' );
-  wp_enqueue_script( 'fitzgerald-routes', plugins_url( 'js/routes.js' , __FILE__ ), array('gmaps', 'backbone.0.9.2') );
-  wp_enqueue_script( 'fitzgerald-views', plugins_url( 'js/views.js', __FILE__ ), array('gmaps', 'backbone.0.9.2') );
+  wp_enqueue_script( 'backbone.0.9.2', plugins_url( 'js/lib/backbone-0.9.2.min.js' , __FILE__ ), array('underscore.1.3.3'), '0.9.2' );
+  wp_enqueue_script( 'fitzgerald-routes', plugins_url( 'js/routes.js' , __FILE__ ), array('gmaps', 'backbone.0.9.2'), '0.4' );
+  wp_enqueue_script( 'fitzgerald-views', plugins_url( 'js/views.js', __FILE__ ), array('gmaps', 'backbone.0.9.2'), '0.4' );
 
   // this one right here needs to be localized!
-  wp_enqueue_script( 'fitzgerald-display', plugins_url( 'js/display.js', __FILE__ ), array('fitzgerald-views') );
+  wp_enqueue_script( 'fitzgerald-display', plugins_url( 'js/display.js', __FILE__ ), array('fitzgerald-views'), '0.4' );
   wp_localize_script( 'fitzgerald-display', 'settings', $localization); //pass any php settings to javascript
   
   // ie conditional
